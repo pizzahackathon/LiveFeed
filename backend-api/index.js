61b5abe396531576fff74e1ff2d80ed346672caf
Bank <zent.bank@gmail.com>
2018-08-20 23:01:05 +0700

    revise endpoint and add token

--- backend-api/index.js
+++ backend-api/index.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const router = require('./router')
 
 const app = express();
+const port = 3000
  
 app.use(cors({origin: true}));
 app.use('/', router);
 
-app.listen(3000);
+app.listen(port);
 
+console.log('todo list RESTful API server started on: ' + port);
